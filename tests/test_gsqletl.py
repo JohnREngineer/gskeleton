92dd61542ee44c06fd9eaf99e3880868330f8bf1
John R <JohnREngineer@gmail.com>
2022-03-22 10:36:17 -0700

    Initial commit

---
+++ tests/test_gsqletl.py
@@ -0,0 +1,11 @@
+from gsqletl import __version__
+from gsqletl.math import multiply_two_numbers
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+def test_multiply_two_numbers():
+    result = multiply_two_numbers(2, 3)
+    assert result == 6
