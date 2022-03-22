2dbb3f1aa698b4e49f2f54b3716b47f246797927
John R <JohnREngineer@gmail.com>
2022-03-22 11:12:38 -0700

    Add pydantic

--- gsqletl/math.py
+++ gsqletl/math.py
@@ -1,6 +1,13 @@
 from numbers import Real
 from typing import Union
 
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    name = "Jane Doe"
+
 
 def multiply_two_numbers(
     a: Union[int, Real], b: Union[int, Real]
--- tests/test_gsqletl.py
+++ tests/test_gsqletl.py
@@ -1,5 +1,5 @@
 from gsqletl import __version__
-from gsqletl.math import multiply_two_numbers
+from gsqletl.math import User, multiply_two_numbers
 
 
 def test_version():
@@ -9,3 +9,9 @@ def test_version():
 def test_multiply_two_numbers():
     result = multiply_two_numbers(2, 3)
     assert result == 6
+
+
+def test_user():
+    user = User(id="123")
+    assert user.id == 123
+    assert user.name == "Jane Doe"
