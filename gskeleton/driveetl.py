6dc856e2aecc3b490e6efdfa2a01c1fc468c6d8a
John R <JohnREngineer@gmail.com>
2022-03-22 19:54:33 -0700

    Add setting credentials from application default

--- gskeleton/driveetl.py
+++ gskeleton/driveetl.py
@@ -1,4 +1,8 @@
+import gspread
+from oauth2client.client import GoogleCredentials
 from pydantic import BaseModel
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
 
 class DriveLocation(BaseModel):
@@ -7,4 +11,12 @@ class DriveLocation(BaseModel):
 
 class DriveETL:
     def __init__(self, settings_key: str):
+        self._set_credentials_from_application_default()
         self.settings_location = DriveLocation(key=settings_key)
+
+    def _set_credentials_from_application_default(self) -> None:
+        self.default_credentials = GoogleCredentials.get_application_default()
+        self.spread = gspread.authorize(self.default_credentials)
+        gauth = GoogleAuth()
+        gauth.credentials = self.default_credentials
+        self.drive = GoogleDrive(gauth)
