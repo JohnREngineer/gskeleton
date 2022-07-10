76e3dabfb6945708ed5c3973cd8dddc60e516e34
John R <JohnREngineer@gmail.com>
2022-07-10 01:18:24 -0700

    Limit the number of files

--- gskeleton/drive_etl.py
+++ gskeleton/drive_etl.py
@@ -122,7 +122,9 @@ class DriveETL:
         sorted_files = sorted(
             filtered, key=(lambda x: x[fs.order_by]), reverse=fs.desc
         )
-        return_files = [GFile(key=f["id"]) for f in sorted_files][fs.top :]
+        return_files = [GFile(key=f["id"]) for f in sorted_files]
+        if fs.top:
+            return_files = return_files[: fs.top]
         return return_files
 
     def _download_drive_file(self, file: GFile) -> str:
